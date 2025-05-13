86069db1ef1e706b654f4851ca84d6a9cda5b8d7
jueunkordev <jueunkordev@gmail.com>
2025-05-13 20:26:14 +0900

    프로젝트 재설정

--- kr.co.codea/src/main/java/kr/co/codea/member/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package kr.co.codea.member;
-
-import org.apache.ibatis.annotations.Mapper;
-import java.util.List;
-
-@Mapper
-public interface MemberMapper {
-    List<Member> findAllMembers();
-}
