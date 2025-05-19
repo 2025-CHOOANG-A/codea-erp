57c89c8026df2c263f2b5e13420c69dad6074aa5
mino1045 <minyoungoh1045@gmail.com>
2025-05-19 20:53:43 +0900

    25.05.19 학원 작업

---
+++ kr.co.codea/src/main/java/kr/co/codea/client/ClientMapper.java
@@ -0,0 +1,12 @@
+package kr.co.codea.client;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ClientMapper {
+    List<ClientDTO> selectAllPartners();
+    ClientDTO selectPartnerWithContacts(int bpId);
+
+}
