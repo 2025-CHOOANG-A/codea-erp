5ea706d24a7e3ec71c281ba0eb8e7e66a7d51f71
HappySmile52 <alice050777@naver.com>
2025-05-21 09:08:02 +0900

    250520

---
+++ kr.co.codea/src/main/java/kr/co/codea/inventory/InventoryService.java
@@ -0,0 +1,9 @@
+package kr.co.codea.inventory;
+
+import java.util.List;
+
+public interface InventoryService {
+	//List<InventoryDTO> inv_select(InventoryDTO dto);
+	
+	//int inv_write(InventoryDTO dto);	// 등록 페이지
+}
