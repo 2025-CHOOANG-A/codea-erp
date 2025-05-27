52818b624bab9fae6ad91f6aef4381785b8d75a8
jueunkordev <jueunkordev@gmail.com>
2025-05-27 15:40:13 +0900

    ✨ feat: 발주 목록 페이징

--- kr.co.codea/src/main/java/kr/co/codea/purchase/PurchaseService.java
+++ kr.co.codea/src/main/java/kr/co/codea/purchase/PurchaseService.java
@@ -3,8 +3,7 @@ package kr.co.codea.purchase;
 import java.util.List;
 import java.util.Map;
 
-
 public interface PurchaseService {
-	List<PurchaseDto> getPurchaseList(Map<String, Object> params);
-    int getPurchaseListCount(Map<String, Object> params);
+    List<PurchaseDto> getPurchaseList(Map<String, Object> params);
+    int getPurchaseListCount(Map<String, Object> params); // 페이징을 위한 전체 개수
 }
